5ec4cc846c2f058dabf8a095059b01c893c04d7d
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-01-17 00:49:21 +0700

    publish message when create, update, and delete product
    handle product liked event

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from products.models import Product, User
+from products.producer import publish
 from products.serializers import ProductSerializer
 import random
 
@@ -18,6 +19,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
     
@@ -31,12 +33,14 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         
     
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
+        publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 class UserAPIView(APIView):
